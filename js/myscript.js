b5cb3b1684a64065fb631642e91468fdcf12e4ca
TH Chen <tzonghao.chen@nist.gov>
2017-12-04 15:39:48 -0500

    Add entry form submission

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,29 @@
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+// On Load
+$(function () {
+  
+  // Initialize Brython
+  brython();
+
+  // Submit entry
+  $("#myform").submit(function (event) {
+    var fields = $(this).serializeArray();
+    var contact = [
+       { name: "Name", value: ''},
+       { name: "Email", value: ''},
+       { name: "Institution", value: ''},
+       { name: "Country/State", value: '', }];
+    var body;
+    body = _.join(
+      _.map(contact, function(f) { return f.name + ": " + f.value; }), '\n'
+    )
+    body += '\n\n';
+    body += _.join(
+      _.map(fields, function(f) { return f.name + ": " + f.value; }), '\n'
+    )
+    event.preventDefault();
+    link = "mailto:TreeHRR2017@nistgov.onmicrosoft.com" +
+           "?subject=" + escape("HRR Competition 2017") +
+           "&body=" + escape(body);
+    window.location.href = link
+  })
+})
